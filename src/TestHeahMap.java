6fb550238c542d06b5769f5ce0f4bb8d05b4cdcc
SalovS <sviatoslavsalov@yandex.ru>
2020-12-17 20:00:08 +0600

    lesson 7 LinkedHashMap TreeMap

--- src/TestHeahMap.java
+++ src/TestHeahMap.java
@@ -1,20 +1,37 @@
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class TestHeahMap {
     public static void main(String[] agrs) {
-        Map<Integer, String> map = new HashMap<>();
+//        Map<String, String> translation = new HashMap<>();
+//
+//        translation.put("кошка", "cat");
+//        translation.put("собака", "dog");
+//        translation.put("слон", "elephant");
+//
+//        for (Map.Entry<String, String> entry : translation.entrySet()) {
+//            System.out.println(entry.getKey() + " : " + entry.getValue());
+//        }
+        Map<Integer, String> hashMap = new HashMap<>();// не никакого порядка
+        Map<Integer, String> linkedHashMap = new LinkedHashMap<>();// устанавливается порядок по дабавлению
+        Map<Integer, String> treeMap = new TreeMap<>();// автоматическая сортировка по ключу
 
-        map.put(1, "Единица");
-        map.put(2, "Двойка");
-        map.put(3, "Тройка");
-//        System.out.println(map);
-//
-//        map.put(3,"Другое значение для ключа три");
-//        System.out.println(map);
-//
-//        System.out.println(map.get(1));
-//        System.out.println(map.get(10));
+        testMap(hashMap);
+        System.out.println();
+        testMap(linkedHashMap);
+        System.out.println();
+        testMap(treeMap);
+    }
+
+    public static void testMap(Map<Integer, String> map) {
+        map.put(39, "Bob");
+        map.put(12, "Mike");
+        map.put(78, "Tom");
+        map.put(0, "Tim");
+        map.put(1500, "Lewis");
+        map.put(7, "Bob");
 
         for (Map.Entry<Integer, String> entry : map.entrySet()) {
             System.out.println(entry.getKey() + " : " + entry.getValue());
